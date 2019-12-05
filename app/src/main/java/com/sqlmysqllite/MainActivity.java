9b4c00c7aec9ac6c0c026715ff924896f59e84f6
manishkhadka189 <mkhadka14.mk@gmail.com>
2019-12-05 12:02:21 +0545

    Initial commit

---
+++ app/src/main/java/com/sqlmysqllite/MainActivity.java
@@ -0,0 +1,19 @@
+package com.sqlmysqllite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+// private EditText word, meaning;
+// private Button btnAdd
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        word=findViewById(R.id.)
+
+    }
+}
